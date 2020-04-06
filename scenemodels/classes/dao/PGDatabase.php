d2e8ba69ef521e09e22a9e9ae0c49d1ee0624285
Torsten Dreyer <torsten@t3r.de>
2020-04-06 13:41:28 +0200

    Don't require ssl but prefer

--- scenemodels/classes/dao/PGDatabase.php
+++ scenemodels/classes/dao/PGDatabase.php
@@ -8,7 +8,7 @@ class PGDatabase implements Database {
     public function __construct($dbname, $dbhost, $dbport, $dbuser, $dbpass) {
 
         $this->connection = pg_connect('dbname='.$dbname.' host='.$dbhost.' port='.$dbport.
-                         ' user='.$dbuser.' password='.$dbpass.' sslmode=require');
+                         ' user='.$dbuser.' password='.$dbpass.' sslmode=prefer');
     }
     
     public function query($query) {
